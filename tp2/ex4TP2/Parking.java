0773733be0ea47d86330ead21497866354950588
Client9@EST.LAN <Client9@EST.LAN>
2019-12-04 10:24:50 +0000

    javaNet

---
+++ tp2/ex4TP2/Parking.java
@@ -0,0 +1,27 @@
+package ex4TP2;
+
+import java.util.List;
+
+public class Parking {
+	private static int NbPlaces;
+	private static List<Voiture> voitures;
+
+	public boolean Enter(){
+		if (voitures.size()<NbPlaces)
+		return true;
+		return false;
+	}
+
+	public void Quitter(){
+		System.out.println("la voiture sort");
+	}
+
+	public void AfficherListeVoitures(){
+		for(int i=0; i<voitures.size(); i++){
+			System.out.println("la voiture "+i);
+			voitures.get(i).toString();
+		}
+
+	}
+
+}
